392231a85b1f925854d262168ea55148286b7942
nfeist55 <nfeist55@gmail.com>
2021-10-06 17:57:58 -0400

    Lets save this here for now

---
+++ C#_Fundamentals/Rest/BarebonesEverythingStore/BBEUI/UI.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace BBEUI
+{
+    static void Main(string[] args)
+    {
+        bool repeat = true;
+        while (repeat)
+        {
+             Console.Clear();
+             Console.WriteLine("Howdy ya'll! You made it to the Barebones Everything Store!");
+             
+        }
+    }
+}
---
+++ C#_Fundamentals/Rest/BarebonesEverythingStore/BBEmodels/Models.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace models
+{
+    public class BarebonesEverythingStore
+    {
+        private string _item;
+        
+            public string Items
+            {
+                get { return _item;}
+                set
+                {
+                    if (!Regex.IsMatch(value, @"^[A-Za-z .]+$"))
+                    {
+                        throw new Exception ("City can only hold letters");
+                    }
+                    _item = value;
+                }
+            }
+            public string brandName {get; set;}
+            public string productName {get;set;}
+            
+        }
+    }
+
